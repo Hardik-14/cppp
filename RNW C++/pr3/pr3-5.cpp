bfae8f70badbcf0e1de8150f367650e27e4ebad3
Hardik-14 <121165610+Hardik-14@users.noreply.github.com>
2023-04-17 15:43:37 +0530

    Update pr3-5.cpp

--- RNW C++/pr3/pr3-5.cpp
+++ RNW C++/pr3/pr3-5.cpp
@@ -1,50 +1,37 @@
 #include<iostream>
 using namespace std;
 
-class MyClass 
-{
-    private:
-
-        int private;
-        int public;
-
-    public:
-        int public;
-        
-        void setPrivate(int num) 
+class num{
+	protected :
+		int a;
+	public :
+		num()
 		{
-            private = num;
-        }
-        
-        int getPrivate()
-		 {
-            return private;
-        }
-        
-        void setPublic(int num) 
-		{
-            public = num;
-        }
-        
-        int getPublic()
-		 {
-            return publicVar;
-        }
+			cout << "Enter A : ";
+			cin >> a;
+		}
 };
 
-int main() {
-    
-	MyClass obj;
-    
-    obj.setPrivate(10);
-    obj.setPublic(20);
-    
-    cout << "Private variable: " << obj.getPrivate() << endl;
-    
-	cout << "Public variable: " << obj.getPublic() << endl;
-    
-	
-    
-    return 0;
-}
+class square  : public num{
+	public :
+		void getsquare()
+		{
+			cout << "A : " << a*a;
+		}
+};
+class cube  : public num{
+	public :
+		void getcube()
+		{
+			cout << "A : " << a*a*a;
+		}
+};
 
+int main()
+{
+	cube c;
+	c.getcube();
+	cout << endl;
+	square s;
+	s.getsquare();
+}
